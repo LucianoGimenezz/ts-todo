1d275199b4503103b9a5f666207fc53344e8652a
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2023-01-06 15:58:49 -0300

    [ADD] Manejo de rutas

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import "./style.css";
 import { addTodo } from "./addTodo";
 import { renderTodo } from "./todos";
 
+export const filterByPending = document.querySelector("#pending");
+export const filterByCompleted = document.querySelector("#completed");
+export const filterByAll = document.querySelector("#all");
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <section class="container">
       <h1>Add Todos</h1>
@@ -13,12 +17,37 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </section>
 `;
 const form = document.querySelector<HTMLFormElement>(".container__addTodo");
-
 form?.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   addTodo(e.target as HTMLFormElement);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  renderTodo();
+window.addEventListener("hashchange", () => {
+  if (window.location.hash.startsWith("#/pending")) {
+    renderTodo();
+    filterByPending?.classList.add("selected");
+    if (filterByAll?.classList.contains("selected")) {
+      filterByAll?.classList.remove("selected");
+    } else {
+      filterByCompleted?.classList.remove("selected");
+    }
+  } else if (window.location.hash.startsWith("#/completed")) {
+    renderTodo();
+    filterByCompleted?.classList.add("selected");
+    if (filterByAll?.classList.contains("selected")) {
+      filterByAll.classList.remove("selected");
+    } else {
+      filterByPending?.classList.remove("selected");
+    }
+  } else {
+    renderTodo();
+    filterByAll?.classList.add("selected");
+    if (filterByCompleted?.classList.contains("selected")) {
+      filterByCompleted?.classList.remove("selected");
+    } else {
+      filterByPending?.classList.remove("selected");
+    }
+  }
 });
+
+renderTodo();
--- src/todos.ts
+++ src/todos.ts
@@ -1,8 +1,42 @@
 import { TodosInterface, StorageNameEnum } from "./todos/todos.model";
 import { deleteTodo, checkTodo } from "./modifyTodo";
+import { filterByAll, filterByCompleted, filterByPending } from "./main";
+
+const taskList = document.querySelector<HTMLDivElement>("#taskList");
+
+function createElement(item: TodosInterface, index: number) {
+  const div = document.createElement("div");
+  div.classList.add("todoItem");
+  const title = document.createElement("p");
+  title.classList.add("todoItem__title");
+  title.innerHTML = item.title;
+  const hr = document.createElement("hr");
+  const todoItemContent = document.createElement("div");
+  todoItemContent.classList.add("todoItem__content");
+  const task = document.createElement("p");
+  task.classList.add("todoItem__task");
+  task.innerHTML = item.task;
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button__container");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("btn", "button__container--delete", `${index}`);
+  buttonDelete.innerHTML = "Delete";
+  const buttonCheck = document.createElement("button");
+  buttonCheck.classList.add("btn", "button__container--check", `${index}`);
+  buttonCheck.innerHTML = "Check";
+  if (item.check) {
+    buttonCheck.classList.add("button__container--checked");
+    buttonCheck.innerHTML = "Checked";
+  }
+
+  buttonContainer.append(buttonDelete, buttonCheck);
+  todoItemContent.append(task);
+  div.append(title, hr, todoItemContent, buttonContainer);
+
+  taskList?.append(div);
+}
 
 export function renderTodo() {
-  const taskList = document.querySelector<HTMLDivElement>("#taskList");
   //Elimino lo que este en el DOM para evitar duplicar contenido
   taskList != null ? (taskList.innerHTML = "") : null;
   const dataLocalStorage: string | null = localStorage.getItem(
@@ -13,49 +47,51 @@ export function renderTodo() {
   if (dataLocalStorage !== null) {
     todos = JSON.parse(dataLocalStorage);
     if (todos?.length === 0 && taskList != null) {
-      document.querySelector<HTMLElement>(".footer")?.classList.add("hidden")
+      document.querySelector<HTMLElement>(".footer")?.classList.add("hidden");
       taskList.innerHTML = "No hay Todos";
     } else if (taskList != null) {
-      document.querySelector<HTMLElement>(".footer")?.classList.remove("hidden")
-      todos?.forEach((item: TodosInterface, index) => {
-        const div = document.createElement("div");
-        div.classList.add("todoItem");
-        const title = document.createElement("p");
-        title.classList.add("todoItem__title");
-        title.innerHTML = item.title;
-        const hr = document.createElement("hr");
-        const todoItemContent = document.createElement("div");
-        todoItemContent.classList.add("todoItem__content");
-        const task = document.createElement("p");
-        task.classList.add("todoItem__task");
-        task.innerHTML = item.task;
-        const buttonContainer = document.createElement("div");
-        buttonContainer.classList.add("button__container");
-        const buttonDelete = document.createElement("button");
-        buttonDelete.classList.add(
-          "btn",
-          "button__container--delete",
-          `${index}`
-        );
-        buttonDelete.innerHTML = "Delete";
-        const buttonCheck = document.createElement("button");
-        buttonCheck.classList.add(
-          "btn",
-          "button__container--check",
-          `${index}`
-        );
-        buttonCheck.innerHTML = "Check";
-        if (item.check) {
-          buttonCheck.classList.add("button__container--checked");
-          buttonCheck.innerHTML = "Checked";
+      document
+        .querySelector<HTMLElement>(".footer")
+        ?.classList.remove("hidden");
+      if (window.location.hash.startsWith("#/pending")) {
+        filterByPending?.classList.add("selected");
+        if (filterByAll?.classList.contains("selected")) {
+          filterByAll?.classList.remove("selected");
+        } else {
+          filterByCompleted?.classList.remove("selected");
         }
-
-        buttonContainer.append(buttonDelete, buttonCheck);
-        todoItemContent.append(task);
-        div.append(title, hr, todoItemContent, buttonContainer);
-
-        taskList.append(div);
-      });
+      } else if (window.location.hash.startsWith("#/completed")) {
+        filterByCompleted?.classList.add("selected");
+        if (filterByAll?.classList.contains("selected")) {
+          filterByAll.classList.remove("selected");
+        } else {
+          filterByPending?.classList.remove("selected");
+        }
+      } else {
+        filterByAll?.classList.add("selected");
+        if (filterByCompleted?.classList.contains("selected")) {
+          filterByCompleted?.classList.remove("selected");
+        } else {
+          filterByPending?.classList.remove("selected");
+        }
+      }
+      if (window.location.hash.startsWith("#/pending")) {
+        todos?.forEach((item: TodosInterface, index) => {
+          if (!item.check) {
+            createElement(item, index);
+          }
+        });
+      } else if (window.location.hash.startsWith("#/completed")) {
+        todos?.forEach((item: TodosInterface, index) => {
+          if (item.check) {
+            createElement(item, index);
+          }
+        });
+      } else {
+        todos?.forEach((item: TodosInterface, index) => {
+          createElement(item, index);
+        });
+      }
 
       const button = document.querySelectorAll<HTMLButtonElement>(
         ".button__container--delete"
@@ -77,8 +113,8 @@ export function renderTodo() {
   } else {
     localStorage.setItem(StorageNameEnum.TODOS, JSON.stringify([]));
     if (taskList != null) {
-      document.querySelector<HTMLElement>(".footer")?.classList.add("hidden")
+      document.querySelector<HTMLElement>(".footer")?.classList.add("hidden");
       taskList.innerHTML = "No hay Todos";
-    } 
+    }
   }
 }
